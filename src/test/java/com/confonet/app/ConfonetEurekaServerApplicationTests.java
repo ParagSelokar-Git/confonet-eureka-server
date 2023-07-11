7f1004c270600b40316deadf37a644efdcb06d32
Parag Selokar <parag.selokar@e-zest.in>
2023-07-11 16:56:11 +0530

    initial code setup

---
+++ src/test/java/com/confonet/app/ConfonetEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confonet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfonetEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
